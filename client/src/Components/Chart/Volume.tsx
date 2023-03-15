b61c18bc707f752b7a7ec21f980097946387f219
Russ <yunsubin481@gmail.com>
2023-03-15 21:31:08 +0900

    volume

--- client/src/Components/Chart/Volume.tsx
+++ client/src/Components/Chart/Volume.tsx
@@ -1,21 +1,61 @@
+import { useState } from "react"
+
 type RtdArr = {
     addRtd:Array<Array<number|string>>
     rtd:Array<Array<number>>
     vol:Array<number>
     }
 const Volume:React.FC<RtdArr> =({rtd,addRtd,vol}) => {
-
+    const [pointer, setPointer] = useState({x:0,y:0}) 
     const height = 160;
     const numYTicks = 4;
     let width = Number(document.body.querySelector('.candle')?.clientWidth)
     const x0 = 10;
-    const y0 = 10;
+    const y0 = 0;
     const yAxisLength = height - 20;
     const xAxisLength = width - 80;
+    const handleMouseMove=(e:any)=>{
+        setPointer({
+            x: e.clientX+10,
+            y: e.clientY-10
+        })
+    }
+
+    const dataArray=[]
+    // for (let i = 0; i < volTo.length; i++) {
+    //     dataArray.push([
+    //         i,
+    //         volTo[i],
+    //         open[i],
+    //         close[i]
+    //     ]
+    //     );
+    // }
+    dataArray[dataArray.length] = [
+        // dataArray.length,
+        // currentPrice.totalVolTo,
+        // currentPrice.open,
+        // currentPrice.close
+    ];
+
+    const dataYMax = dataArray.reduce(
+        (max, [_, vol]) => Math.max(vol, /*현재 거래량 */ max),
+        -Infinity
+        
+    );
+    let SVG_VOLUME_WIDTH = typeof width === "number" ? width * 1 : 0;
+    let SVG_VOLUME_HEIGHT = typeof height === "number" ? height * 1 : 0;
+
+    let windowPageYOffset = window.pageYOffset
+    console.log(pointer)
 
     return(
     <div className="volume">
-        <svg width={'100%'} height={height} >
+        <svg 
+        width={'100%'} 
+        height={height} 
+        onMouseMove={handleMouseMove}
+        >
             
             {/*----기본 가로 선----*/}
             <line
@@ -45,7 +85,7 @@ const Volume:React.FC<RtdArr> =({rtd,addRtd,vol}) => {
                         x2={x0}
                         y1={y}
                         y2={y}
-                        stroke='#121212'
+                        stroke='#323232'
                     ></line>
                     <text
                         x={width}
@@ -58,7 +98,35 @@ const Volume:React.FC<RtdArr> =({rtd,addRtd,vol}) => {
                     </g>
                 );
             })}
-            {/*----시세 세로 선----*/}
+            {/* ----------포인터-----------735 895 */}
+            <line
+                x1={pointer.x<SVG_VOLUME_WIDTH-70&&((pointer.y+windowPageYOffset)<875)?pointer.x-10:-20}
+                x2={pointer.x<SVG_VOLUME_WIDTH-70&&((pointer.y+windowPageYOffset)<875)?pointer.x-10:-20}
+                y1={0}
+                y2={SVG_VOLUME_HEIGHT-20}
+                stroke='#00fbff'
+                opacity={0.3}
+                ></line>
+                <line
+                x1={0}
+                x2={SVG_VOLUME_WIDTH-70}
+                y1={pointer.x+10<SVG_VOLUME_WIDTH-60&&((pointer.y+windowPageYOffset)<875)?((pointer.y+windowPageYOffset)-735):-20}
+                y2={pointer.x+10<SVG_VOLUME_WIDTH-60&&((pointer.y+windowPageYOffset)<875)?((pointer.y+windowPageYOffset)-735):-20}
+                stroke='#00fbff'
+                opacity={0.3}
+                ></line>
+                <text
+                x={SVG_VOLUME_WIDTH-60}
+                y={pointer.x+10<SVG_VOLUME_WIDTH-60&&
+                ((pointer.y+windowPageYOffset)<875)?
+                ((pointer.y+windowPageYOffset)-735):-20}
+                fill='#00fbff'
+                stroke='#00fbff'
+                opacity={0.5}
+                fontSize='12px'
+            > 
+            {(Number(dataYMax)*(700-Number(pointer.y))/120).toFixed(1).toLocaleString()}
+                </text>
             {/*----시세 세로 선----*/}
             {/*----시세 세로 선----*/}
             {/*----시세 세로 선----*/}
