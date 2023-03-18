e656716237abeab748f9c1bbb452900bd355325c
Russ <yunsubin481@gmail.com>
2023-03-18 20:21:14 +0900

    volume

--- client/src/Components/Chart/Volume.tsx
+++ client/src/Components/Chart/Volume.tsx
@@ -1,52 +1,57 @@
+import { scaleLinear } from "d3-scale"
 import { useState } from "react"
 
 type RtdArr = {
     addRtd:Array<Array<number|string>>
     rtd:Array<Array<number>>
     vol:Array<number>
+    open:Array<Array<number>>
+    close:Array<Array<number>>
     }
-const Volume:React.FC<RtdArr> =({rtd,addRtd,vol}) => {
+const Volume:React.FC<RtdArr> =({rtd,addRtd,vol,open,close}) => {
     const [pointer, setPointer] = useState({x:0,y:0}) 
     const height = 160;
     const numYTicks = 4;
     let width = Number(document.body.querySelector('.candle')?.clientWidth)
-    const x0 = 10;
-    const y0 = 0;
+    const x0 = 0;
+    const y0 = 10;
     const yAxisLength = height - 20;
     const xAxisLength = width - 80;
-    const handleMouseMove=(e:{clientX:number;clientY:number}) =>{        setPointer({
+    const handleMouseMove=(e:{clientX:number;clientY:number}) =>{
+        setPointer({
             x: e.clientX+10,
             y: e.clientY-10
         })
     }
-    const dataArray=[]
-    // for (let i = 0; i < volTo.length; i++) {
-    //     dataArray.push([
-    //         i,
-    //         volTo[i],
-    //         open[i],
-    //         close[i]
-    //     ]
-    //     );
-    // }
+    const dataArray:any[]=[]
+    for (let i = 0; i < vol.length; i++) {
+        dataArray.push([
+            i,
+            vol[i],
+            open[i],
+            close[i]
+        ]
+        );
+    }
+    // console.log(rtd[rtd.length-1])
     dataArray[dataArray.length] = [
-        // dataArray.length,
-        // currentPrice.totalVolTo,
-        // currentPrice.open,
-        // currentPrice.close
+        dataArray.length,
+        rtd[rtd.length-1],
+        rtd[0][0],
+        rtd[0][rtd[0].length-1]
     ];
-
     const dataYMax = dataArray.reduce(
-        (max, [_, vol]) => Math.max(vol, /*현재 거래량 */ max),
+        (max, [_, vol,open,close]) => Math.max(vol, max),
         -Infinity
-        
     );
     let SVG_VOLUME_WIDTH = typeof width === "number" ? width * 1 : 0;
     let SVG_VOLUME_HEIGHT = typeof height === "number" ? height * 1 : 0;
 
+    const dataYMin = 0
+    const dataYRange = dataYMax;
+    const barPlothWidth = xAxisLength / dataArray.length>0?xAxisLength / dataArray.length:0.001;
+    const currentFill = rtd[0][0]>rtd[0][rtd[0].length-1] ? "#b8284a" : "#00A4D8" ;
     let windowPageYOffset = window.pageYOffset
-    // console.log(pointer)
-
     return(
     <div className="volume">
         <svg 
@@ -74,29 +79,32 @@ const Volume:React.FC<RtdArr> =({rtd,addRtd,vol}) => {
 
             {/*----시세 세로 선----*/}
             {Array.from({ length: numYTicks }).map((_, index) => {
-            const y = y0 + index * (yAxisLength / numYTicks);
+                const y = y0 + index * (yAxisLength / numYTicks);
+                const yValue = (
+                    dataYMax - index * (dataYRange / numYTicks)
+                );
+                
                 return (
                     <g key={index}>
                     <line
                         className="lineLight"
-                        x1={xAxisLength-1}
+                        x1={xAxisLength}
                         x2={x0}
                         y1={y}
                         y2={y}
                         stroke='#323232'
                     ></line>
                     <text
-                        x={width}
-                        y={y}
-                        textAnchor="middle"
-                        fontSize={Number(document.body.querySelector('.volume')?.clientWidth) < 800 ? 6 : 10}
-                        stroke='#323232'
-                    >
-                        {/* {xValue[index]} */}
+                        className='select_ven'
+                        x={SVG_VOLUME_WIDTH - 60} 
+                        y={y} 
+                        fontSize="10" 
+                        stroke='#323232' >
+                        {yValue.toFixed(2)} 
                     </text>
                     </g>
-                );
-            })}
+                )})};
+
             {/* ----------포인터-----------735 895 */}
             <line
                 x1={pointer.x<SVG_VOLUME_WIDTH-70&&((pointer.y+windowPageYOffset)<875)?pointer.x-10:-20}
@@ -124,10 +132,66 @@ const Volume:React.FC<RtdArr> =({rtd,addRtd,vol}) => {
                 opacity={0.5}
                 fontSize='12px'
             > 
-            {(Number(dataYMax)*(700-Number(pointer.y))/120).toFixed(1).toLocaleString()}
+            {(dataYMax*(875-pointer.y)/160).toFixed(1).toLocaleString()}
                 </text>
-            {/*----시세 세로 선----*/}
-            {/*----시세 세로 선----*/}
+
+            {/*----거래량----*/}
+            {dataArray.map(
+                ([index, vol, open, close]) => {
+                    const x = x0 + index * barPlothWidth;
+                    const sidePadding = xAxisLength * 0.0015;
+                    let yRatio = 0;
+                    const yRatioGenerator = () => {
+                        yRatio = (vol - dataYMin)*0.9 / dataYMax;
+                        if (yRatio > 0) {
+                        return yRatio;
+                        } else return (yRatio = vol / dataYRange);
+                    };
+                    const y = y0 + (1 - yRatioGenerator()) * yAxisLength-10;
+                    const height = yRatioGenerator() * yAxisLength;
+                    const fill = open>close ? "#b8284a" : "#00A4D8" ;
+                    
+                    return (                 
+                    <g key={index}>
+                        <rect
+                        {...{ fill }}
+                        x={x}
+                        y={y-0.5}
+                        width={(barPlothWidth - sidePadding)>0?barPlothWidth - sidePadding:0.01}
+                        height={height}
+                        ></rect>
+                        {index===dataArray.length-1?
+                            <g key={index}>
+                                <line
+                                    x1={xAxisLength+10}
+                                    x2={x0}
+                                    y1={y-0.5}
+                                    y2={y-0.5}
+                                    strokeWidth='0.1'
+                                    stroke={currentFill}
+                                    >
+                                    </line>
+                                    <text
+                                    x={SVG_VOLUME_WIDTH - 60}
+                                    y={y-0.5}
+                                    fontSize="12" 
+                                    fill={currentFill} 
+                                    >
+                                    {Number(rtd[rtd.length-1]).toFixed(2)}
+                                </text>
+                            </g>:<></>
+                        }
+                    </g>
+                    
+                    );
+                    
+                }
+                
+            )}
+            {/*----시세 선----*/}
+
+
+
             {/*----시세 세로 선----*/}
             
         </svg>    
